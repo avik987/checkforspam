b1b5f871f54105a177363e760aedc32a65602420
Avik <avik.torosyan987@gmail.com>
2017-08-10 19:10:18 +0400

    blacklist ciew

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 // Site base URL
 global $site_base_url;
 //$site_base_url = "www.checkforspam.com/";  //live url
-$site_base_url = "http://checkforspam.loc/"; //local
+$site_base_url = "http://spam.loc/"; //local
 
 // Spam servers
 global $spam_blacklist_servers;
@@ -18,9 +18,9 @@ $spam_blacklist_servers = array(
     'ManiTu' => 'ix.dnsbl.manitu.net',
     'AbuseAt' => 'cbl.abuseat.org',
     'ASPEWS' => "www.aspews.org",
-    'Abuse IP' => "rbl.abuse.ro",
-    'Abuse Domain' => "dbl.abuse.ro",
-    'Anonmails DNSBL' => "spam.dnsbl.anonmails.de",
+    'AbuseIP' => "rbl.abuse.ro",
+    'AbuseDomain' => "dbl.abuse.ro",
+    'AnonmailsDNSBL' => "spam.dnsbl.anonmails.de",
 
 //    'AntiCaptcha.NET IPv6' => "",
 //    'BACKSCATTERER' => "ips.backscatterer.org",
@@ -28,193 +28,189 @@ $spam_blacklist_servers = array(
 //    'BBFHL1' => "www.bbfh.org",
 //    'BBFHL2' => "www.bbfh.org",
 
-    'BLOCKLIST.DE' => "all.bl.blocklist.de",
+    'BLOCKLISTDE' => "all.bl.blocklist.de",
     'BSB' => "bsb.empty.us",
 
     'CALIVENT' => "dnsbl.calivent.com.pe",
     'CASA' => "cbl.anti-spam.org.cn",
     'CBL' => "cbl.abuseat.org",
-    'CYMRU BOGONS' => "v4.fullbogons.cymru.com",
-    'CYMRU BOGONS IPv6' => "v6.fullbogons.cymru.com",
-    'DAN TOR' => "tor.dan.me.uk",
-    'DAN TOREXIT' => "torexit.dan.me.uk",
-    'Dns Realtime Blackhole List' => 'dnsrbl.org',
+    'CYMRUBOGONS' => "v4.fullbogons.cymru.com",
+    'CYMRUBOGONSIPv6' => "v6.fullbogons.cymru.com",
+    'DANTOR' => "tor.dan.me.uk",
+    'DANTOREXIT' => "torexit.dan.me.uk",
+    'DnsRealtimeBlackholeList' => 'dnsrbl.org',
 
-    'DMARC External Validation' => undefined,
-    'DMARC Multiple Records' => undefined,
+//    'DMARC External Validation' => undefined,
+//    'DMARC Multiple Records' => undefined,
 
-    'DMARC Record Published' => "tools.ietf.org/html/rfc7489",
+    'DMARCRecordPublished' => "tools.ietf.org/html/rfc7489",
 
-    'DMARC Syntax Check' => undefined,
-    'DNS All Name Servers Timed Out' => undefined,
+//    'DMARC Syntax Check' => undefined,
+//    'DNS All Name Servers Timed Out' => undefined,
 
-    'DNS All Servers Authoritative' => "www.ietf.org/rfc/rfc1034.txt",
+    'DNSAllServersAuthoritative' => "www.ietf.org/rfc/rfc1034.txt",
 
-    'DNS All Servers Responding' => undefined,
+//    'DNS All Servers Responding' => undefined,
 
-    'DNS At Least Two Servers' => "tools.ietf.org/html/rfc2182",
-    'DNS Bad Glue Detected' => "tools.ietf.org/html/rfc1033",
+    'DNSAtLeastwoServers' => "tools.ietf.org/html/rfc2182",
+    'DNSBadGlueDetected' => "tools.ietf.org/html/rfc1033",
 
-    'DNS Local Parent Mismatch' => undefined,
-    'DNS Lookup Timeout' => undefined,
-    'DNS Open Recursive Name Server' => undefined,
-    'DNS Open Zone Transfer' => undefined,
+//    'DNS Local Parent Mismatch' => undefined,
+//    'DNS Lookup Timeout' => undefined,
+//    'DNS Open Recursive Name Server' => undefined,
+//    'DNS Open Zone Transfer' => undefined,
 
-    'DNS Primary Server Listed At Parent' => "mxtoolbox.com/problem/dns/dns-local-parent-mismatch",
-    'DNS Realtime Blackhole List' => "dnsrbl.org/",
+   // 'DNSPrimaryServerListedAtParent' => "mxtoolbox.com/problem/dns/dns-local-parent-mismatch",
+    //'DNSRealtimeBlackholeList' => "dnsrbl.org/",
 
-    'DNS Record Published' => undefined,
+//    'DNS Record Published' => undefined,
 
-    'DNS SERVICIOS' => "rbl.dns-servicios.com",
-    'DNS SOA Expire Value' => "mxtoolbox.com/problem/dns/dns-all-servers-authoritative",
-    'DNS SOA NXDOMAIN Value' => "tools.ietf.org/html/rfc2308",
-    'DNS SOA Refresh Value' => "www.ietf.org/rfc/rfc1912.txt",
-    'DNS SOA Retry Value' => "www.ietf.org/rfc/rfc1912.txt",
+    'DNSSERVICIOS' => "rbl.dns-servicios.com",
+//    'DNSSOAExpireValue' => "mxtoolbox.com/problem/dns/dns-all-servers-authoritative",
+//    'DNSSOANXDOMAINValue' => "tools.ietf.org/html/rfc2308",
+//    'DNSSOARefreshValue' => "www.ietf.org/rfc/rfc1912.txt",
+//    'DNSSOARetryValue' => "www.ietf.org/rfc/rfc1912.txt",
 
-    'DNS SOA Serial Number Format' => undefined,
-    'DNS SOA Serial Numbers Match' => undefined,
-    'DNS Server Allows Zone Transfer' => undefined,
+//    'DNS SOA Serial Number Format' => undefined,
+//    'DNS SOA Serial Numbers Match' => undefined,
+//    'DNS Server Allows Zone Transfer' => undefined,
 
-    'DNS Servers Have Public IP Addresses' => "en.wikipedia.org/wiki/Reserved_IP_addresses",
-    'DNS Servers are on Different Subnets' => "tools.ietf.org/html/rfc2182",
+//    'DNSServersHavePublicIPAddresses' => "en.wikipedia.org/wiki/Reserved_IP_addresses",
+//    'DNSServersareonDifferentSubnets' => "tools.ietf.org/html/rfc2182",
 
-    'DOMAIN DNS Failure' => undefined,
-    'DOMAIN Monitors Change' => undefined,
+//    'DOMAIN DNS Failure' => undefined,
+//    'DOMAIN Monitors Change' => undefined,
 
     'DRMX' => " bl.drmx.org",
-    'DRONE BL' => "dnsbl.dronebl.org",
+    //'DRONEBL' => "dnsbl.dronebl.org",
 //    'DULRU' => "www.dul.ru/",
-    'Domain Expiration Check' => undefined,
+//    'Domain Expiration Check' => undefined,
     'FABELSOURCES' => "spamsources.fabel.dk",
 //    'HIL' => "www.habeas.com/supportWhiteList.html",
 //    'HIL2' => "www.habeas.com",
-    'HTTP Connect' => "mxtoolbox.com/problem/http/http-dns",
-    'HTTP Delay Check' => "mxtoolbox.com/problem/http/http-connect",
-    'HTTP Dns' => "mxtoolbox.com/problem/http/http-connect",
-    'HTTP Filter' => "mxtoolbox.com/problem/http/http-connect",
-    'HTTPS Certificate Check' => "mxtoolbox.com/problem/https/https-certificate-expiration",
-    'HTTPS Certificate Expiration' => "mxtoolbox.com/Public/UpgradeV2.aspx",
+//    'HTTPConnect' => "mxtoolbox.com/problem/http/http-dns",
+//    'HTTPDelay Check' => "mxtoolbox.com/problem/http/http-connect",
+//    'HTTPDns' => "mxtoolbox.com/problem/http/http-connect",
+//    'HTTPFilter' => "mxtoolbox.com/problem/http/http-connect",
+//    'HTTPSCertificate Check' => "mxtoolbox.com/problem/https/https-certificate-expiration",
+//    'HTTPSCertificate Expiration' => "mxtoolbox.com/Public/UpgradeV2.aspx",
 
-    'IBM DNS Blacklist' => "dnsbl.cobion.com",
+    'IBMDNSBlacklist' => "dnsbl.cobion.com",
     'ICMFORBIDDEN' => "dnsrbl.swinog.ch",
-    'IMP SPAM' => "antispam.imp.ch/?lng=1",
-    'IMP WORM' => "antispam.imp.ch/?lng=1",
+//    'IMPSPAM' => "antispam.imp.ch/?lng=1",
+//    'IMPWORM' => "antispam.imp.ch/?lng=1",
 //    'INPS_DE' => "dnsbl.inps.de/index.cgi?lang=en&site=00001",
     'INTERSERVER' => "rbldata.interserver.net",
-    'IPrange RBL Project' => "rbl.realtimeblacklist.com",
+    'IPrangeRBLProject' => "rbl.realtimeblacklist.com",
     'JIPPG' => "dialup.blacklist.jippg.org",
     'KEMPTBL' => "dnsbl.kempt.net",
 //    'KISA' => "www.kisarbl.or.kr/english/", dns does not know
     'Konstant' => "bl.konstant.no",
-    'LASHBACK' => "www.lashback.com/blacklist/",
-    'LNSGBLOCK' => "www.leadmon.net/spamguard",
-    'LNSGBULK' => "www.leadmon.net/spamguard",
-    'LNSGMULTI' => "www.leadmon.net/spamguard",
-    'LNSGOR' => "www.leadmon.net/spamguard",
-    'LNSGSRC' => "www.leadmon.net/spamguard",
-    'MADAVI' => "www.madavi.de/index.php?id=39&type=0",
-    'MAILSPIKE BL' => "mailspike.net",
-    'MAILSPIKE Z' => "mailspike.net",
-    'MSRBL Phishing' => "msrbl.blogspot.com/",
-    'MSRBL Spam' => "msrbl.blogspot.com/",
-    'MailBlacklist' => "mailblacklist.com/",
-    'NETHERRELAYS' => "puck.nether.net/or/",
-    'NETHERUNSURE' => "puck.nether.net/or/",
-    'NIXSPAM' => "www.heise.de/ix/NiX-Spam-DNSBL-and-blacklist-for-download-499637.html",
-    'NoSolicitado' => "www.nosolicitado.org/",
-    'ORVEDB' => "www.aupads.org/",
-    'OSPAM' => "0spam.fusionzero.com/",
-    'PSBL' => "psbl.org/",
-    'RATS Dyna' => "www.spamrats.com",
-    'RATS NoPtr' => "www.spamrats.com",
-    'RATS Spam' => "www.spamrats.com",
-    'RBL JP' => "www.rbl.jp/allrbl-e.html",
-    'RSBL' => "www.aupads.org/",
-    'SCHULTE' => "rbl.schulte.org/",
-    'SECTOOR EXITNODES' => "www.sectoor.de/tor.php",
-    'SEM BACKSCATTER' => "spameatingmonkey.com/index.html",
-    'SEM BLACK' => "spameatingmonkey.com/index.html",
+//    'LASHBACK' => "www.lashback.com/blacklist/",
+    'LNSGBLOCK' => "spamguard.leadmon.net",
+    'LNSGBULK' => "spamguard.leadmon.net",
+    'LNSGMULTI' => "spamguard.leadmon.net",
+    'LNSGOR' => "spamguard.leadmon.net",
+    'LNSGSRC' => "spamguard.leadmon.net",
+    'MADAVI' => "pop3.madavi.de",
+    'MAILSPIKEBL' => "rep.mailspike.net",
+    'MAILSPIKEZ' => "z.mailspike.net",
+    'MSRBLPhishing' => "mx.fakemx.net",
+    'MSRBLSpam' => "mx.fakemx.net",
+//    'MailBlacklist' => "mailblacklist.com/",  dns does not know
+//    'NETHERRELAYS' => "puck.nether.net", ????
+//    'NETHERUNSURE' => "puck.nether.net", ???
+    'NIXSPAM' => "ix.dnsbl.manitu.net",
+    //  'NoSolicitado' => "www.nosolicitado.org/", ???
+    'ORVEDB' => "rsbl.aupads.org",
+    'OSPAM' => "0spam.fusionzero.com",
+    'PSBL' => "psbl.surriel.com",
+    'RATSDyna' => "dyna.spamrats.com",
+    'RATSNoPtr' => "noptr.spamrats.com",
+    'RATSSpam' => "spam.spamrats.com",
+    'RBLJP' => "RBL.JP",
+    'RSBL' => "rsbl.aupads.org",
+    'SCHULTE' => "cbl.abuseat.org, etc",
+    'SECTOOREXITNODES' => "www.sectoor.de/tor.php",
+    'SEMBACKSCATTER' => "backscatter.spameatingmonkey.net",
+    'SEMBLACK' => "backscatter.spameatingmonkey.net",
 
-    'SEM FRESH' => undefined,
-    'SEM URI' => undefined,
+//    'SEM FRESH' => undefined,
+//    'SEM URI' => undefined,
 
-    'SEM URIRED' => "mxtoolbox.com/Public/BlacklistDetails.aspx?bl=SEM-URI",
-    'SERVICESNET' => "korea.services.net/",
+    //'SEMURIRED' => "mxtoolbox.com/Public/BlacklistDetails.aspx?bl=SEM-URI",
+    'SERVICESNET' => "korea.services.net",
 
-    'SMTP Banner Check' => undefined,
+//    'SMTP Banner Check' => undefined,
 
-    'SMTP Connect' => "technet.microsoft.com/en-us/library/bb123891.aspx#TF",
-    'SMTP Connection Time' => "technet.microsoft.com/en-us/library/bb123891.aspx#TF",
-    'SMTP DNS Resolution' => "mxtoolbox.com/DNSLookup.aspx",
+    'SMTPConnect' => "technet.microsoft.com/en-us/library/bb123891.aspx#TF",
+    'SMTPConnectionTime' => "technet.microsoft.com/en-us/library/bb123891.aspx#TF",
+    'SMTPDNSResolution' => "mxtoolbox.com/DNSLookup.aspx",
 
-    'SMTP Open Relay' => undefined,
+//    'SMTP Open Relay' => undefined,
 
-    'SMTP Reverse DNS Mismatch' => "mxtoolbox.com/supertool.aspx?action=a=>smtp.mxtoolbox.com",
-    'SMTP Reverse DNS Resolution' => "mxtoolbox.com/ReverseLookup.aspx",
+    'SMTPReverseDNSMismatch' => "mxtoolbox.com/supertool.aspx?action=a=>smtp.mxtoolbox.com",
+    'SMTPReverseDNSResolution' => "mxtoolbox.com/ReverseLookup.aspx",
 
-    'SMTP Server Disconnected' => undefined,
-    'SMTP TLS' => undefined,
+//    'SMTP Server Disconnected' => undefined,
+//    'SMTP TLS' => undefined,
 
-    'SMTP Transaction Time' => "technet.microsoft.com/en-us/library/bb123891.aspx#TF",
-    'SORBS BLOCK' => "www.sorbs.net/lookup.shtml",
-    'SORBS DUHL' => "www.sorbs.net/lookup.shtml",
-    'SORBS HTTP' => "www.sorbs.net/lookup.shtml",
-    'SORBS MISC' => "www.sorbs.net/lookup.shtml",
-    'SORBS NEW' => "mxtoolbox.com/problem/blacklist/sorbs-spam",
+    'SMTPTransactionTime' => "technet.microsoft.com/en-us/library/bb123891.aspx#TF",
+    'SORBSBLOCK' => "block.dnsbl.sorbs.net",
+    'SORBSDUHL' => "dul.dnsbl.sorbs.net",
+    'SORBSHTTP' => "http.dnsbl.sorbs.net",
+    'SORBSMISC' => "misc.dnsbl.sorbs.net",
+    'SORBSNEW' => "new.spam.dnsbl.sorbs.net",
 
-    'SORBS RHSBL BADCONF' => undefined,
-    'SORBS RHSBL NOMAIL' => undefined,
+//    'SORBS RHSBL BADCONF' => undefined,
+//    'SORBS RHSBL NOMAIL' => undefined,
 
-    'SORBS SMTP' => "www.sorbs.net/lookup.shtml",
-    'SORBS SOCKS' => "www.sorbs.net/lookup.shtml",
-    'SORBS SPAM' => "www.sorbs.net/lookup.shtml",
-    'SORBS WEB' => "www.sorbs.net/lookup.shtml",
-    'SORBS ZOMBIE' => "www.sorbs.net/lookup.shtml",
-    'SPAMCANNIBAL' => "www.spamcannibal.org/",
-    'SPAMCOP' => "spamcop.net/bl.shtml",
-    'SPEWS1' => "www.spews.org/",
-    'SPEWS2' => "www.spews.org/",
-    'SPF Included Lookups' => "tools.ietf.org/html/rfc7208",
-    'SPF Multiple Records' => "tools.ietf.org/html/rfc7208",
-    'SPF Record Deprecated' => "tools.ietf.org/html/rfc7208",
+    'SORBSSMTP' => "smtp.dnsbl.sorbs.net",
+    'SORBSSOCKS' => "socks.dnsbl.sorbs.net",
+    'SORBSSPAM' => "spam.dnsbl.sorbs.net",
+    'SORBSWEB' => "web.dnsbl.sorbs.net",
+    'SORBSZOMBIE' => "zombie.dnsbl.sorbs.net",
+//    'SPAMCANNIBAL' => "www.spamcannibal.org/",  dns does not know
+   // 'SPAMCOP' => "bl.spamcop.net",
+//    'SPEWS1' => "www.spews.org/", ???
+//    'SPEWS2' => "www.spews.org/", ???
+    'SuomispamReputation'=>'dbl.suomispam.net',
+    //'SPF Included Lookups' => "tools.ietf.org/html/rfc7208",
+    //'SPF Multiple Records' => "tools.ietf.org/html/rfc7208",
+    //'SPF Record Deprecated' => "tools.ietf.org/html/rfc7208",
 
-    'SPF Record Published' => undefined,
-    'SPF Syntax Check' => undefined,
-    'SPF Test Result' => undefined,
-    'SURBL multi' => undefined,
 
-    'SWINOG' => "antispam.imp.ch/?lng=1",
-    'Sender Score Reputation Network' => "www.senderscore.org/landing/index.php?campid=701000000006WWq&redirect=/register",
-    'Spam Eating Monkey SEM IPv6BL' => "spameatingmonkey.com/",
 
-    'Spamhaus DBL' => undefined,
+    'SWINOG' => "dnsrbl.swinog.ch",
+//    'Sender Score Reputation Network' => "www.senderscore.org/landing/index.php?campid=701000000006WWq&redirect=/register", ???
+    'SpamEatingMonkeySEMIPv6BL' => "uribl.spameatingmonkey.net",
 
-    'Spamhaus ZEN' => "www.spamhaus.org",
-    'Suomispam Reputation' => "suomispam.net/",
+    'SpamhausZEN' => "zen.spamhaus.org",
 
-    'TCP Connect' => undefined,
 
-    'TCP Delay Check' => "mxtoolbox.com/problem/http/tcp-connect",
+//    'TCP Connect' => undefined,
 
-    'TCP Dns' => undefined,
+    //'TCP Delay Check' => "mxtoolbox.com/problem/http/tcp-connect",
 
-    'TRIUMF' => "trmail.triumf.ca/cgi-bin/rbl2/",
-    'TRUNCATE' => "www.gbudb.com/truncate/index.jsp",
-    'UCEPROTECTL1' => "www.uceprotect.net/",
-    'UCEPROTECTL2' => "www.uceprotect.net/",
-    'UCEPROTECTL3' => "www.uceprotect.net/",
-    'VIRBL' => "virbl.bit.nl/",
-    'Virbl IPv6' => "virbl.bit.nl/",
-    'WPBL' => "www.wpbl.info/",
-    'Woodys SMTP Blacklist' => "blacklist.woody.ch/rblcheck.php3",
-    'Woodys SMTP Blacklist IPv6' => "blacklist.woody.ch/rblcheck.php3",
-    'ZapBL' => "zapbl.net/",
-//    'ivmSIP' => "dnsbl.invaluement.com/ivmsip/",
-//    'ivmSIP24' => "dnsbl.invaluement.com/ivmsip24/",
+//    'TCP Dns' => undefined,
 
-    'ivmURI' => undefined,
+    'TRIUMF' => "rbl2.triumf.ca",
+    'TRUNCATE' => "truncate.gbudb.net",
+    'UCEPROTECTL1' => "dnsbl-1.uceprotect.net ",
+    'UCEPROTECTL2' => "dnsbl-2.uceprotect.net",
+    'UCEPROTECTL3' => "dnsbl-3.uceprotect.net ",
+    'VIRBL' => "virbl.dnsbl.bit.nl",
+    'VirblIPv6' => "virbl.bit.nl",
+    'WPBL' => "db.wpbl.info",
+//    'Woodys SMTP Blacklist' => "blacklist.woody.ch/rblcheck.php3",   dns does not know
+//    'Woodys SMTP Blacklist IPv6' => "blacklist.woody.ch/rblcheck.php3",   dns does not know
+    'ZapBL' => "dnsbl.zapbl.net",
+   'ivmSIP' => "dnsbl.invaluement.com/ivmsip/",
+    'ivmSIP24' => "dnsbl.invaluement.com/ivmsip24/",
 
-    's5h.net IPv6' => "www.usenix.org.uk/content/rbl.html",
+    //'ivmURI' => undefined,
+
+    's5hnetIPv6' => "all.s5h.net",
 );
 
 
@@ -229,7 +225,7 @@ $db_servername = "localhost";
 //$db_password = "mk#w9%ww??r~";
 // local db access
 $db_username = "root";
-$db_password = "123123";
+$db_password = "123";
 $db_name = "checkfor_main";
 
 ?>
